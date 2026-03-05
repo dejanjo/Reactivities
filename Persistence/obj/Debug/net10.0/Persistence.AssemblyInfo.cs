55268bf78f4da532297fd1f86629a1fd072287b0
Dejan Jovanovic <dejanjovanovicofficial@gmail.com>
2026-03-05 10:15:41 +0100

    Add api controllers

--- Persistence/obj/Debug/net10.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net10.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f43cbcd5da4c8e6ec73159e014083eadcaf91676")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
